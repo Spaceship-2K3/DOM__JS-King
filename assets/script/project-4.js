51564209f45bf7b8ac0dd2229e914abb2de910e7
DV.Sang <vansangIT2k3@gmai.com>
2023-09-16 21:05:34 +0700

    create project 4

--- assets/script/project-4.js
+++ assets/script/project-4.js
@@ -1,1 +1,12 @@
+const textAreaEl = document.querySelector("#textarea");
+const totalCounterEl = document.querySelector(".total-counter");
+const remainCounterEl = document.querySelector(".remaining-counter");
 
+textAreaEl.addEventListener("keyup", updateCounter);
+
+function updateCounter() {
+    // todo : get max length of textarea
+    const maxLength = parseInt(textAreaEl.getAttribute("maxlength"));
+    totalCounterEl.innerHTML = textAreaEl.value.length;
+    remainCounterEl.innerHTML = maxLength - parseInt(totalCounterEl.innerHTML);
+}
